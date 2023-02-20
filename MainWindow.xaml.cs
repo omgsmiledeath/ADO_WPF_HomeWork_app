f5f7441dc665cda79d70963a20d58ceb6fefcb1f
Holiday <araragikyn@gmail.com>
2023-02-20 13:35:04 +0800

    OUTPUT OleDB in OrdersGird

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
 using System.Data.OleDb;
+using System.Data;
+using System.Runtime.CompilerServices;
+
 
 namespace ADO_WPF_HomeWork_app
 {
@@ -22,13 +25,66 @@ namespace ADO_WPF_HomeWork_app
     /// </summary>
     public partial class MainWindow : Window
     {
-        private SqlDataAdapter sqlAdapter;
-        private OleDbDataAdapter
+        #region Переменные
+        bool isConnectedToAccess;
+        bool isConnectedToSql;
+        private SqlDataAdapter sqlAdapter = new SqlDataAdapter();
+        private SqlConnection sqlCon=new SqlConnection();
+        private OleDbDataAdapter OleDbAdapter = new OleDbDataAdapter();
+        private OleDbConnection oleDbCon = new OleDbConnection();
+        private DataTable custumersDt = new DataTable();
+        private DataTable ordersDt = new DataTable();
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
-        
+        public bool ConnectToAccess (string path)
+        {
+            var OleDBStringBuilder = new OleDbConnectionStringBuilder();
+            OleDBStringBuilder.DataSource = @"D:\OrdersBase.accdb;DataBase Password = '123'";
+            OleDBStringBuilder.Provider = "Microsoft.ACE.OLEDB.12.0";
+           
+
+
+            oleDbCon.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source = D:\\OrdersBase.accdb; Jet OLEDB:DataBase Password = '123'";
+            try
+            {
+                oleDbCon.Open();
+                if(oleDbCon.State==ConnectionState.Open) 
+                {
+                    MessageBox.Show("Connection to Access base is ok");
+                    isConnectedToAccess= true;
+                   
+                    
+                    var scom = new OleDbCommand("SELECT * FROM Orders");
+                    OleDbAdapter = new OleDbDataAdapter("SELECT * FROM Orders", oleDbCon);
+                    OleDbAdapter.Fill(ordersDt);
+                    OrdersGrid.ItemsSource = ordersDt.DefaultView;
+                    //var com = new OleDbCommand(@"INSERT INTO Orders (email,productId,productDescription)
+                    //                             VALUES('1@1.ru',2,'чтото');", oleDbCon);
+                    //com.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("Connection to Access base its not ok");
+                    isConnectedToAccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "ERROR");
+                isConnectedToAccess = false;
+            }
+                        return isConnectedToAccess;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectToAccess("");
+        }
     }
 }
